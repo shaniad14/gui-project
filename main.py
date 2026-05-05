f385e59d5992c92986a0e134afba1e5f5cbd63d9
shaniad14 <shaniad1414@gmail.com>
2026-05-04 19:50:39 -0700

    added the function for calculating the bmi

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ import tkinter as tk
 # Set up window
 root = tk.Tk()
 root.title("BMI Calculator")
+
+# Function to calculate BMI
+def calculate_bmi():
+    mass = float(entry_mass.get())
+    height = scale_height.get()
+    
+    bmi = round(mass / (height ** 2), 2)
+    
+    result = f"BMI: {bmi}"
