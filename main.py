be528d456189c9669ac2406790cb992a302c2655
shaniad14 <shaniad1414@gmail.com>
2026-05-04 20:04:21 -0700

    restarted

--- main.py
+++ main.py
@@ -1,38 +1,24 @@
-"""
-BMI calculator GUI
-"""
-
 import tkinter as tk
 
-# Set up window
 root = tk.Tk()
-root.title("BMI Calculator")
+root.title("BMI")
 
-# Function to calculate BMI
-def calculate_bmi():
-    mass = float(entry_mass.get())
-    height = scale_height.get()
+def calc():
+    m = float(e1.get())
+    h = s1.get()
     
-    bmi = round(mass / (height ** 2), 2)
+    bmi = round(m / (h*h), 2)
     
-    result = f"BMI: {bmi}"
+    text = "BMI: " + str(bmi)
+    
+    if g.get() == "Male":
+        text += " Male"
+    if g.get() == "Female":
+        text += " Female"
+        
+    if a.get() == 1:
+        text += " Athlete"
+    
+    l3["text"] = text
 
-     # Add gender
-    result += f" | Gender: {gender.get()}"
-    
-    # Check athlete box
-    if athlete.get() == 1:
-        result += " | Athlete"
-    
-    label_result.config(text=result)
-
-    # Mass input
-tk.Label(root, text="Mass (kg):").grid(row=0, column=0)
-entry_mass = tk.Entry(root)
-entry_mass.grid(row=0, column=1)
-
-# Height slider
-scale_height = tk.Scale(root, from_=1.0, to=2.5, resolution=0.01,
-                        orient="horizontal", label="Height (m)")
-scale_height.grid(row=1, column=0, columnspan=2)
 
